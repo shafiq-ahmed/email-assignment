e65212aad937052384b24cc174e5b439d764d81d
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-13 14:30:53 +0600

    Initial commit

---
+++ src/main/java/com/mail/EmailAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAssignmentApplication.class, args);
+	}
+
+}
